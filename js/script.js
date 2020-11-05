7ee24783fc7ba52e2632d9be0b2cc20983e99dbc
sourceCodeISA <isabel.gogov1@libero.it>
2020-11-05 16:05:38 +0100

    creato schema logico

--- js/script.js
+++ js/script.js
@@ -2,3 +2,10 @@
 // Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
 // Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
 
+// creo ciclo for per generare 5 alert
+
+// utilizzo setTimeout per far passare 30 s
+
+// creo ciclo for per generare 5 prompt
+
+// stampo quanti e quali numeri sono stati ricordati
