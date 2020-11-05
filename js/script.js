065ae62dc2b5490da0c8ec5242a140c130cc3be6
sourceCodeISA <isabel.gogov1@libero.it>
2020-11-05 16:26:05 +0100

    ciclo for che genera alert

--- js/script.js
+++ js/script.js
@@ -2,7 +2,17 @@
 // Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
 // Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
 
-// creo ciclo for per generare 5 alert
+var listaN = [];
+
+// creo ciclo for per generare 5 alert che contengono un n randomico
+for (var i = 0; i < 5; i++) {
+    var n = Math.floor(Math.random() * 100) + 1; 
+    alert(n);
+
+    listaN.push(n);
+    
+}
+console.log(listaN);
 
 // utilizzo setTimeout per far passare 30 s
 
