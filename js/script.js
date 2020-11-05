f56cbacab38bb3366c4a15f9522a51296691975b
sourceCodeISA <isabel.gogov1@libero.it>
2020-11-05 17:24:07 +0100

    controllo se i numeri si ripetono

--- js/script.js
+++ js/script.js
@@ -3,16 +3,32 @@
 // Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
 
 var listaN = [], listaU = [];
+var controllo = true;
 
 // creo ciclo for per generare 5 alert che contengono un n randomico
 for (var i = 0; i < 5; i++) {
     var n = Math.floor(Math.random() * 100) + 1; 
-    alert(n);
 
-    listaN.push(n);
-    
+    // controllo
+    while (controllo) {
+        
+        if (!listaN.includes(n)) { // se non si ripete pusha il numero nell'array
+            
+            listaN.push(n);
+
+            controllo = false; // ed esci dal ciclo
+        } else {
+            
+            n = Math.floor(Math.random() * 100) + 1; // altrimenti genera un nuovo n
+        }
+
+    }
+
+    controllo = true;
+    alert(n);
 }
 console.log(listaN);
+
 
 // utilizzo setTimeout per far passare 30 s
 setTimeout(function () {
