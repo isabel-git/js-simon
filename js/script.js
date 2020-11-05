813abf3c53f886ae1c49e2b9b2b3563904180a67
sourceCodeISA <isabel.gogov1@libero.it>
2020-11-05 16:45:23 +0100

    setTimeout e ciclo for per generare i prompt

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 // Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
 // Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
 
-var listaN = [];
+var listaN = [], listaU = [];
 
 // creo ciclo for per generare 5 alert che contengono un n randomico
 for (var i = 0; i < 5; i++) {
@@ -15,7 +15,17 @@ for (var i = 0; i < 5; i++) {
 console.log(listaN);
 
 // utilizzo setTimeout per far passare 30 s
+setTimeout(function () {
+    // console.log("ciao");
+    
+    // creo ciclo for per generare 5 prompt
+    for (var i = 0; i < 5; i++) {
+        var inputUte = parseInt(prompt("inserisci il numero memorizzato"));
+        listaU.push(inputUte);
+    }
+    console.log(listaU);
 
-// creo ciclo for per generare 5 prompt
+}, 3000);
+
 
 // stampo quanti e quali numeri sono stati ricordati
