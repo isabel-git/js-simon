5471423ae49bc091d975cc1cddc0553597311727
sourceCodeISA <isabel.gogov1@libero.it>
2020-11-09 21:30:11 +0100

    controllo input doppioni

--- js/script.js
+++ js/script.js
@@ -2,50 +2,63 @@
 // Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
 // Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
 
+var inputUte;
 var listaN = [], listaU = [];
 var controllo = true;
 
 // creo ciclo for per generare 5 alert che contengono un n randomico
 for (var i = 0; i < 5; i++) {
-    var n = Math.floor(Math.random() * 100) + 1; 
+    var n = Math.floor(Math.random() * 100) + 1;
 
     // controllo
     while (controllo) {
-        
-        if (!listaN.includes(n)) { // se non si ripete pusha il numero nell'array
-            
-            listaN.push(n);
 
+        if (!listaN.includes(n)) { // se n non si ripete pusha il numero nell'array
+            listaN.push(n);
             controllo = false; // ed esci dal ciclo
         } else {
-            
             n = Math.floor(Math.random() * 100) + 1; // altrimenti genera un nuovo n
         }
 
     }
 
     controllo = true;
-    alert(n);
+    alert("Simon dice: " + n);
 }
 console.log(listaN);
 
 
 // utilizzo setTimeout per far passare 30 s
 setTimeout(function () {
-    
+
     // creo ciclo for per generare 5 prompt
+    controllo = true;
     for (var i = 0; i < 5; i++) {
-        var inputUte = parseInt(prompt("inserisci il numero memorizzato"));
-        listaU.push(inputUte);
+
+        inputUte = parseInt(prompt("inserisci il numero memorizzato"));
 
         // controllo
-        if (listaN.includes(inputUte)) { 
-        console.log("bravo! il numero " + inputUte + " e' presente"); // stampa se l'utente ha inserito un numero presente 
+        while (controllo) {
+
+            if (!listaU.includes(inputUte)) { // se non presente pushalo
+                listaU.push(inputUte);
+                controllo = false; // ed esci dal ciclo
+            } else {
+                alert("il numero e' gia stato inserito"); // altrimenti stampa alert e chiedi nuovamente 
+                inputUte = parseInt(prompt("inserisci il numero memorizzato"));
+            }
+
+        }
+        controllo = true;
+
+        // controllo
+        if (listaN.includes(inputUte)) {
+            console.log("bravo! il numero " + inputUte + " e' presente"); // stampa se il numero dell'utente compare nel array
+        } else {
+            console.log("mi dispiace! il numero " + inputUte + " non e' presente"); // stampa che non e' presente
         }
     }
     console.log(listaU);
 
+
 }, 3000);
-
-
-// stampo quanti e quali numeri sono stati ricordati
